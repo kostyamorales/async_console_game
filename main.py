3f096011418b8161e3721e8dfa69817fba612ba8
Konstantin Zemlyakov <kostyamorales@gmail.com>
2021-04-28 17:53:33 +0300

    Add spaceship

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import time
 import curses
 import asyncio
 from random import randint, choice
+from curses_tools import draw_frame
+import itertools
+
+
+async def animate_spaceship(canvas, row, column, model):
+    for frame in itertools.cycle(model):
+        draw_frame(canvas, row, column, frame)
+        canvas.refresh()
+        time.sleep(0.2)
+        await asyncio.sleep(0)
+        draw_frame(canvas, row, column, frame, negative=True)
 
 
 async def blink(canvas, row, column, symbol='*'):
@@ -53,6 +64,14 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         column += columns_speed
 
 
+def read_rocket_frames():
+    with open("files/rocket_frame_1.txt", "r") as my_file:
+        frame_1 = my_file.read()
+    with open("files/rocket_frame_2.txt", "r") as my_file:
+        frame_2 = my_file.read()
+    return frame_1, frame_2
+
+
 def draw(canvas):
     curses.curs_set(False)
     canvas.border()
@@ -68,6 +87,10 @@ def draw(canvas):
 
     coroutine_fire = fire(canvas, height / 2, width / 2)
     coroutines.append(coroutine_fire)
+
+    model = read_rocket_frames()
+    coroutine_spaceship = animate_spaceship(canvas, height / 2, width / 2, model)
+    coroutines.append(coroutine_spaceship)
 
     while True:
         for coroutine in coroutines.copy():
