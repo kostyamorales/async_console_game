d85da4aee808abb471994453616bb2a81c3e5b71
Konstantin Zemlyakov <kostyamorales@gmail.com>
2021-05-03 19:55:53 +0300

    Create a game scenario

--- main.py
+++ main.py
@@ -4,23 +4,37 @@ import asyncio
 from random import randint, choice
 from curses_tools import draw_frame, read_controls, get_frame_size
 import itertools
-from utils import sleep, explode, show_gameover
+from utils import sleep, explode, get_garbage_delay_tics, show_gameover
 from upload_frames import read_trash, read_rocket_frames, read_game_over
 from physics import update_speed
 from obstacles import Obstacle
 
+YEAR = 1957
 TIC_TIMEOUT = 0.1
 COROUTINES = []
 OBSTACLES = []
 OBSTACLES_IN_LAST_COLLISIONS = []
+PHRASES = {
+    1957: "First Sputnik",
+    1961: "Gagarin flew!",
+    1969: "Armstrong got on the moon!",
+    1971: "First orbital space station Salute-1",
+    1981: "Flight of the Shuttle Columbia",
+    1998: 'ISS start building',
+    2011: 'Messenger launch to Mercury',
+    2020: "Take the plasma gun! Shoot the garbage!",
+}
 
 
 async def fill_orbit_with_garbage(canvas, width, frames):
     while True:
-        column = randint(0, width - 1)
-        coroutine_garbage = fly_garbage(canvas, column, choice(frames))
-        COROUTINES.append(coroutine_garbage)
-        await sleep(5)
+        delay = get_garbage_delay_tics(YEAR)
+        if delay:
+            await sleep(delay)
+            column = randint(0, width - 1)
+            coroutine_garbage = fly_garbage(canvas, column, choice(frames))
+            COROUTINES.append(coroutine_garbage)
+        await asyncio.sleep(0)
 
 
 async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
@@ -65,7 +79,7 @@ async def animate_spaceship(canvas, row, column, frames, game_over_frame):
         if 0 < column + column_speed < canvas_columns - frame_columns:
             column += column_speed
 
-        if flag_fire:
+        if flag_fire and YEAR >= 2020:
             COROUTINES.append(fire(canvas, row, column + 2))
 
 
@@ -94,6 +108,17 @@ async def blink(canvas, row, column, symbol='*'):
         await sleep(randint(1, 20))
 
 
+async def count_year(canvas, height, width):
+    global YEAR
+    while True:
+        await sleep(2)
+        YEAR += 1
+        message = f'Year {YEAR} - {PHRASES.get(YEAR, "")}'
+        small_window = canvas.derwin(0, 0)
+        small_window.addstr(height - 2, width - 55, message)
+        await asyncio.sleep(0)
+
+
 def draw(canvas):
     curses.curs_set(False)
     canvas.nodelay(True)
@@ -116,6 +141,9 @@ def draw(canvas):
     garbage_frames = read_trash()
     coroutine_garbage = fill_orbit_with_garbage(canvas, width, garbage_frames)
     COROUTINES.append(coroutine_garbage)
+
+    coroutine_count_year = count_year(canvas, height, width)
+    COROUTINES.append(coroutine_count_year)
 
     while True:
         for coroutine in COROUTINES.copy():
